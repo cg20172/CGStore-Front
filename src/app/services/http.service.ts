6f1d94d52e6d88cdeae5c9afbd1134a491afe167
Miller Correa <macorreag@unal.edu.co>
2017-10-09 02:33:38 -0500

    creacion servicio Http |funcionando|

---
+++ src/app/services/http.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, RequestOptions, Headers } from '@angular/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class HttpService {
+
+  constructor(private http: Http) {}
+    
+getRequest(url: string) {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let requestOptions = new RequestOptions({ headers: headers });
+        
+        return this.http.get(url, requestOptions)
+            .catch(error => {
+                return Observable.throw(error);
+            });
+        
+    }
+
+}
