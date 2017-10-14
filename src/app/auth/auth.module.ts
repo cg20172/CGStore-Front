e6f922f2c6dc7c5ac3d3dca215a7246323b1860e
Edward Carrillo <eccarrilloe@gmail.com>
2017-10-14 01:11:59 -0500

    feat(auth): auth components added

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ForgotComponent } from './forgot/forgot.component';
+import { ResetComponent } from './reset/reset.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LoginComponent]
+  declarations: [LoginComponent, RegisterComponent, ForgotComponent, ResetComponent]
 })
 export class AuthModule { }
