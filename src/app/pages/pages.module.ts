9d4c3a1d2ac513bb390873b1a4b9155957427260
gcantem <gcantem@unal.edu.co>
2017-10-17 05:14:59 -0500

    Added form example

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BrowserModule }       from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';  // <-- #1 import module
+ 
 
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
@@ -7,12 +10,28 @@ import { DoorsComponent } from './products/doors/doors.component';
 import { MachineryComponent } from './products/machinery/machinery.component';
 import { LegosComponent } from './products/legos/legos.component';
 
+import { HeroDetailComponent } from './products/doors/hero-detail.component'; // <-- #1 import component
+import { HeroListComponent }   from './products/doors/hero-list.component';
+
+import { HeroService }         from './products/doors/hero.service';
+
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    BrowserModule,
+    ReactiveFormsModule // <-- #2 add to @NgModule imports
   ],
-  declarations: [HomeComponent, ContactComponent, DoorsComponent, MachineryComponent, LegosComponent],
-  exports: [HomeComponent, ContactComponent, DoorsComponent, MachineryComponent, LegosComponent]
+  declarations: [
+  	HomeComponent, ContactComponent, DoorsComponent, MachineryComponent, LegosComponent,
+  	HeroDetailComponent, // <-- #3 declare app component
+    HeroListComponent
+  ],
+  exports: [
+  	HomeComponent, ContactComponent, DoorsComponent, MachineryComponent, LegosComponent,
+  	HeroDetailComponent,
+    HeroListComponent
+  ],
+  providers: [ HeroService ]
 })
 
 export class PagesModule { }
