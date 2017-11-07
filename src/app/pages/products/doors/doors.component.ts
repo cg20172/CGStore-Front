9e4503dbeea36bedb6a8f41661f0890fb4480ea1
Miller Correa <macorreag@unal.edu.co>
2017-11-07 05:21:11 -0500

    [Puertas] feat slider [Home] terminado los datos e imagenes

--- src/app/pages/products/doors/doors.component.ts
+++ src/app/pages/products/doors/doors.component.ts
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DoorsComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+ }
 
   ngOnInit() {
+
+
   }
+
+
+
 
 }
