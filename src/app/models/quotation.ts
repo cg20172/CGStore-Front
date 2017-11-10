ada6f10d0138bb8226106b9867f4eaa0a6963d31
Miller Correa <macorreag@unal.edu.co>
2017-11-10 14:36:34 -0500

    working in profile

--- src/app/models/quotation.ts
+++ src/app/models/quotation.ts
@@ -5,6 +5,7 @@ export class Quotation {
   public id: number;
   public date: Date;
   public productId: number;
+  public typeProduct: string;
   public quantity: number;
   public userId: number;
   public state: string;
@@ -19,8 +20,10 @@ export class Quotation {
         let date = data.fecha ? new Date(data.fecha) : null;
         this.id = data.pedidoid ? data.pedidoid : null;
         this.productId = data.productoid ? data.productoid : null;
+        this.typeProduct = data.tipoproducto ? data.tipoproducto : null;
+        this.state = data.estado ? data.estado : null;
         this.quantity = data.cantidad ? data.cantidad : null;
-        this.date = data.fecha ? new Date(data.fecha) : null;
+        this.date = data.fecha ? data.fecha : null;
       } else {
         let date = data.date ? new Date(data.date) : null;
         this.id = data.id ? data.id : null;
@@ -28,7 +31,7 @@ export class Quotation {
         this.quantity = data.quantity ? data.quantity : null;
         this.userId = data.userId ? data.userId : null;
         this.state = data.state ? data.state : "1";
-        this.date = data.date ? new Date(data.date) : null;
+        this.date = data.date ? new Date(data.fecha) : null;
         this.createdAt = data.createdAt ? new Date(data.createdAt) : null;
         this.updatedAt = data.updatedAt ? new Date(data.updatedAt) : null;
       }
